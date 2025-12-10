1701fc62f9e4acf820825072d9806a59a04ba858
Jacques <jvw679@gmail.com>
2025-12-10 08:28:27 +0200

    fix ebook download count tracking

--- resources/views/resources/ebooks.blade.php
+++ resources/views/resources/ebooks.blade.php
@@ -75,8 +75,7 @@
                             {{ $featuredEbook->description }}
                         </p>
                         <div class="flex gap-4">
-                            <a href="{{ asset('storage/ebooks/' . $featuredEbook->pdf_file) }}"
-                               download
+                            <a href="{{ route('ebooks.download', $featuredEbook->slug) }}"
                                class="pma-btn pma-btn-primary inline-flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
@@ -134,8 +133,7 @@
                         {{ number_format($ebook->download_count) }} downloads
                     </p>
                     @endif
-                    <a href="{{ asset('storage/ebooks/' . $ebook->pdf_file) }}"
-                       download
+                    <a href="{{ route('ebooks.download', $ebook->slug) }}"
                        class="pma-btn pma-btn-secondary w-full inline-flex items-center justify-center">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
@@ -194,8 +192,7 @@
                         {{ number_format($ebook->download_count) }} downloads
                     </p>
                     @endif
-                    <a href="{{ asset('storage/ebooks/' . $ebook->pdf_file) }}"
-                       download
+                    <a href="{{ route('ebooks.download', $ebook->slug) }}"
                        class="pma-btn pma-btn-secondary w-full inline-flex items-center justify-center">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
