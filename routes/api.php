829b4cfd9479aea290dc0934893daf90774aabf7
Jacques <jvw679@gmail.com>
2025-12-11 13:31:53 +0200

    add video editor API routes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\KieCallbackController;
 use App\Http\Controllers\Api\PledgeController;
+use App\Http\Controllers\Api\VideoEditorController;
 use App\Models\Short;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/kie/callback', [KieCallbackController::class, 'handle'])->name('kie.callback');
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/pledge/update', [PledgeController::class, 'update']);
+});
+
+Route::prefix('video-editor')->group(function () {
+    Route::post('/upload', [VideoEditorController::class, 'upload']);
+    Route::post('/auto-detect', [VideoEditorController::class, 'autoDetect']);
+    Route::post('/export', [VideoEditorController::class, 'export']);
 });
 
 Route::get('/shorts/{short}', function (Short $short) {
