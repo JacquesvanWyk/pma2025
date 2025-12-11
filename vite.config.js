95257405fa77502bddb31e1e2f42feb82da6e1fd
Jacques <jvw679@gmail.com>
2025-12-11 13:32:00 +0200

    add wavesurfer.js dependency for audio waveform

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,34 @@ import {
     defineConfig
 } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 
+                'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/css/filament/admin/theme.css'
+                'resources/css/filament/admin/theme.css',
+                'resources/js/video-editor/main.js',
             ],
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
     server: {
         cors: true,
+    },
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js',
+        },
     },
 });
